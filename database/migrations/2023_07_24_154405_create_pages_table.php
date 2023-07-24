799be7e066f5a2936b3e5cb2d8b69f13b9dcaab0
Abdoul Mouctar DIALLO <abdoulmouctard@gmail.com>
2023-07-24 15:44:47 +0200

    (saas:issue-9): page model genration

---
+++ database/migrations/2023_07_24_154405_create_pages_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        Schema::create('pages', function (Blueprint $table) {
+            $table->id();
+            $table->integer('id');
+            $table->string('slug');
+            $table->string('title');
+            $table->string('description');
+            $table->longText('content');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('pages');
+    }
+};
